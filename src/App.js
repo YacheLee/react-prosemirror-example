f629650e56d87e3d8e3ca38755dd582ae0f05d7c
ya-che.li <ya-che.li@and.digital>
2020-04-19 10:40:40 +0100

    from html as a inital data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, {useEffect, useRef} from 'react';
+import {DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
-import {baseKeymap} from "prosemirror-commands";
-import {history, redo, undo} from "prosemirror-history";
-import {keymap} from "prosemirror-keymap";
 import {EditorState} from "prosemirror-state"
 import {EditorView} from "prosemirror-view"
 import './App.css';
 
 function App() {
     const editor = useRef(null);
+    const content = useRef(null);
 
     useEffect(() => {
         let state = EditorState.create({
-            schema,
-            plugins: [
-                history(),
-                keymap({'Mod-z': undo, 'Mod-y': redo}),
-                keymap(baseKeymap)
-            ]
+            doc: DOMParser.fromSchema(schema).parse(content.current)
         })
         let view = new EditorView(editor.current, {state, dispatchTransaction(transaction){
             console.log('Document size went from ', transaction.before.content.size, 'to', transaction.doc.content.size);
@@ -28,8 +22,14 @@ function App() {
     });
   return (
     <div className="ProseMirror-example-setup-style">
-      <div ref={editor}>
-      </div>
+        <div ref={content}>
+            asdakljl
+            adsada
+            ad
+            嘿嘿嘿
+            安安安
+        </div>
+      <div ref={editor} />
     </div>
   );
 }
