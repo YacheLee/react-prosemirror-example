96326f6d843fabc97638478bd4f247688f8f509d
ya-che.li <ya-che.li@and.digital>
2020-04-20 17:50:11 +0100

    add schema for heading

--- src/components/RichTextEditor/components/Editor/Editor.js
+++ src/components/RichTextEditor/components/Editor/Editor.js
@@ -16,12 +16,29 @@ function Editor({value, onChange}) {
         if(!editorView){
             const schema = new Schema({
                 nodes: {
-                    doc: {content: "paragraph+"},
+                    doc: {content: "block+"},
                     paragraph: {
+                        group: "block",
                         content: "text*",
                         toDOM(node) { return ["p", 0] }
                     },
-                    text: {inline: true}
+                    heading: {
+                        attrs: {level: {default: 1}},
+                        content: "inline*",
+                        group: "block",
+                        defining: true,
+                        parseDOM: [{tag: "h1", attrs: {level: 1}},
+                            {tag: "h2", attrs: {level: 2}},
+                            {tag: "h3", attrs: {level: 3}},
+                            {tag: "h4", attrs: {level: 4}},
+                            {tag: "h5", attrs: {level: 5}},
+                            {tag: "h6", attrs: {level: 6}}],
+                        toDOM(node) { return ["h" + node.attrs.level, 0] }
+                    },
+                    text: {
+                        group: "inline",
+                        inline: true
+                    },
                 },
                 marks: {
                     strong: {
